31b09edaaaf19135c50131d22501f485916c78ad
Alexander Kal <alexjkal@gmail.com>
2025-06-01 22:47:39 -0400

    Create welcomeScript.sh

---
+++ welcomeScript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir ~/Desktop/Onboarding Materials
+
+open -a /Applications/Self\ Service.app/
+
+sleep 3
+
+say -v 'Karen' "Hello New User, and welcome to the Company.  Your Mac is running updates now.  Please do not restart or shut down your Mac."
+
+exit 0
