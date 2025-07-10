e714f231b322eb5b5448effd17a53c5c406503e6
Alexander Kal <alexjkal@gmail.com>
2025-07-10 13:41:18 -0400

    Update verify_Executable.sh

--- verify_Executable.sh
+++ verify_Executable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-script_name="/Users/alex/nudge_SystemSettings_helper.sh" # Replace with the actual script name
+script_name="<your script>" # Replace with the actual script name
 
 if [ -x "$script_name" ]; then
 	echo "The script '$script_name' is executable."
