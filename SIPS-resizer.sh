b345641c706a4ab449bda043b5a63cf74ca261a7
Alexander Kal <alexjkal@gmail.com>
2025-07-10 13:40:01 -0400

    Add files via upload

---
+++ SIPS-resizer.sh
@@ -0,0 +1,13 @@
+logo="<your logo>"
+
+# Resize to specific dimensions
+sips -z 512 512 $logo --out icon_512x512.png
+
+# Resize to multiple sizes at once
+sips -z 16 16 $logo --out sling_icon_16x16.png
+sips -z 32 32 $logo --out sling_icon_32x32.png
+sips -z 64 64 $logo --out sling_icon_64x64.png
+sips -z 128 128 $logo --out sling_icon_128x128.png
+sips -z 256 256 $logo --out sling_icon_256x256.png
+sips -z 512 512 $logo --out sling_icon_512x512.png
+sips -z 1024 1024 $logo --out sling_icon_1024x1024.png
